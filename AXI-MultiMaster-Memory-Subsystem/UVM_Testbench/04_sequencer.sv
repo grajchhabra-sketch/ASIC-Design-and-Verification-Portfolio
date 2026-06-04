ffaf988c31fd4f709775b21c8d7cf1519d4475f1
Gautam Raj Chhabra <grajchhabra@gmail.com>
2026-06-04 13:15:05 +0530

    Add axi_sequencer class to UVM testbench

---
+++ AXI-MultiMaster-Memory-Subsystem/UVM_Testbench/04_sequencer.sv
@@ -0,0 +1,13 @@
+class axi_sequencer extends
+uvm_sequencer #(axi_txn);
+
+`uvm_component_utils(axi_sequencer)
+
+function new(string name,
+uvm_component parent);
+
+super.new(name,parent);
+
+endfunction
+
+endclass
